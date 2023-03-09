f3a4161d45559c9a91af8dab224e25aaacda2733
Kiana <salmasikiana@gmail.com>
2023-03-09 10:12:18 -0600

    Updated the file by adding some code to the "square" and "item" in stylesheet

--- Task.js
+++ Task.js
@@ -1,34 +1,40 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView, TouchableOpacity} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 const Task = (props) => {
 
    return (
-   <ScrollView >
       <View style = {styles.item}>
           <View style={styles.itemLeft}>
-            <TouchableOpacity style = {styles.square}>
+            <TouchableOpacity style = {styles.square}></TouchableOpacity>
               <Text style = {styles.itemLeft}>{props.text}</Text>
-            </TouchableOpacity>
           </View>
-         <View style= {styles.circular}>
-
-         </View>
+         <View style= {styles.circular}></View>
       </View>
-    </ScrollView>
     );
-  };
+  }
 
   const styles = StyleSheet.create({
     item: {
         backgroundColor: '#FFF',
         padding: 15,
         borderRadius: 10,
-    },
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 20,        
+        justifyContent: 'space-between'
+        },
     itemLeft: {
-
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap'
     },
     square: {
-
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15
     },
     itemText: {
 
