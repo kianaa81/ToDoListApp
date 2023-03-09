5cac6f9c194926b8ee4ccb05a86d98d10792bc74
Kiana <salmasikiana@gmail.com>
2023-03-08 23:33:04 -0600

    Created a Task.js for users to add their tasks

---
+++ Task.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {View, Text, StyleSheet, ScrollView} from 'react-native';
+const Task = () => {
+
+   return (
+      <View>
+        <ScrollView >
+            <Text>Hi</Text>
+          </ScrollView>
+      </View>
+    );
+  };
+
+  const styles = StyleSheet.create({
+
+  });
+
+export default Task;
